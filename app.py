d6935ab188dec4c70f1de274368771006c1d5a2b
git-arm <111350495+git-arm@users.noreply.github.com>
2022-08-17 19:24:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == "__main__":
